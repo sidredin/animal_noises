1785941c12ed1cc75ef834ad698e88e1381103b4
dizelvinable <sadr3479@gmail.com>
2021-06-13 11:20:41 +0300

    Gridview instead of that Column

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(AnimalNoisesApp());
 
 class AnimalNoisesApp extends StatelessWidget {
-  final int namesInRow = 3;
   final animalNames = [
     'sheep',
     'goat',
@@ -31,55 +30,28 @@ class AnimalNoisesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final animalNamesMatrix = listToMatrix(animalNames, namesInRow);
-
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Column(
-            children: getAnimalsGrid(animalNamesMatrix),
+          child: GridView.count(
+            crossAxisCount: 3,
+            children: getAnimalsList(animalNames),
           ),
         ),
       ),
     );
   }
 
-  List<List<String>> listToMatrix(List<String> animalNames, int namesInRow) {
-    final rowsNum = (animalNames.length / namesInRow).ceil();
-    List<List<String>> animalNamesMatrix = [];
-    List<String> row = [];
-
-    for (var i = 0; i <= rowsNum; i++) {
-      for (var j = i * namesInRow; j < animalNames.length; j++) {
-        row.add(animalNames[j]);
-        if ((j + 1) % namesInRow == 0 || j == animalNames.length - 1) {
-          animalNamesMatrix.add(row);
-          row = [];
-          break;
-        }
-      }
+  getAnimalsList(List<String> animalNames) {
+    List<Animal> animalsList = [];
+    for (var name in animalNames) {
+      animalsList.add(Animal(
+        name: name,
+        player: player,
+        imagesDir: imagesDir,
+      ));
     }
-    return animalNamesMatrix;
-  }
-
-  getAnimalsGrid(List<List<String>> animalNamesMatrix) {
-    List<Row> animalsGrid = [];
-    for (var row in animalNamesMatrix) {
-      List<Animal> rowChildren = [];
-      for (var name in row) {
-        rowChildren.add(Animal(
-          name: name,
-          player: player,
-          imagesDir: imagesDir,
-        ));
-      }
-      animalsGrid.add(
-        Row(
-          children: rowChildren,
-        ),
-      );
-    }
-    return animalsGrid;
+    return animalsList;
   }
 }
 
@@ -92,17 +64,15 @@ class Animal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: TextButton(
-        style: TextButton.styleFrom(
-          padding: EdgeInsets.zero,
-        ),
-        onPressed: () {
-          player.play('$name.wav');
-        },
-        child: Image.asset(
-          '$imagesDir/$name.jpg',
-        ),
+    return TextButton(
+      style: TextButton.styleFrom(
+        padding: EdgeInsets.zero,
+      ),
+      onPressed: () {
+        player.play('$name.wav');
+      },
+      child: Image.asset(
+        '$imagesDir/$name.jpg',
       ),
     );
   }
