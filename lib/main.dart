90c703f9080dd4b94f6c92b42721022767245a08
dizelvinable <sadr3479@gmail.com>
2021-06-13 05:53:58 +0300

    Animals grid added

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,105 @@ import 'package:flutter/material.dart';
 void main() => runApp(AnimalNoisesApp());
 
 class AnimalNoisesApp extends StatelessWidget {
-  final int imagesInRow = 2;
+  final int namesInRow = 3;
+  final animalNames = [
+    'sheep',
+    'goat',
+    'horse',
+    'cow',
+    'dog',
+    'cat',
+    'goose',
+    'duck',
+    'cock',
+    'hen',
+    'chicks',
+    'turkey',
+    'crow',
+    'wolf',
+    'lion',
+    'tiger',
+    'frog',
+    'pig',
+  ];
+  final imagesDir = 'images';
+
+  final AudioCache player = AudioCache();
 
   @override
   Widget build(BuildContext context) {
+    final animalNamesMatrix = listToMatrix(animalNames, namesInRow);
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: getAnimalsGrid(animalNamesMatrix),
+          ),
         ),
       ),
     );
   }
+
+  List<List<String>> listToMatrix(List<String> animalNames, int namesInRow) {
+    final rowsNum = (animalNames.length / namesInRow).ceil();
+    List<List<String>> animalNamesMatrix = [];
+    List<String> row = [];
+
+    for (var i = 0; i <= rowsNum; i++) {
+      for (var j = i * namesInRow; j < animalNames.length; j++) {
+        row.add(animalNames[j]);
+        if ((j + 1) % namesInRow == 0 || j == animalNames.length - 1) {
+          animalNamesMatrix.add(row);
+          row = [];
+          break;
+        }
+      }
+    }
+    return animalNamesMatrix;
+  }
+
+  getAnimalsGrid(List<List<String>> animalNamesMatrix) {
+    List<Row> animalsGrid = [];
+    for (var row in animalNamesMatrix) {
+      List<Animal> rowChildren = [];
+      for (var name in row) {
+        rowChildren.add(Animal(
+          name: name,
+          player: player,
+          imagesDir: imagesDir,
+        ));
+      }
+      animalsGrid.add(
+        Row(
+          children: rowChildren,
+        ),
+      );
+    }
+    return animalsGrid;
+  }
 }
 
 class Animal extends StatelessWidget {
+  final String imagesDir;
   final String name;
   final AudioCache player;
-  const Animal({Key key, this.name, this.player}) : super(key: key);
+  const Animal({Key key, this.name, this.player, this.imagesDir = 'images'})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {
-        player.play('$name.wav');
-      },
-      child: Image.asset(
-        '$name.jpg',
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero,
+        ),
+        onPressed: () {
+          player.play('$name.wav');
+        },
+        child: Image.asset(
+          '$imagesDir/$name.jpg',
+        ),
       ),
     );
   }
